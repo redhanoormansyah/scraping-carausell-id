6ceef33da8de40ce5506342d3393bac004c96c2c
redhanoormansyah <redhanoormansyah30@gmail.com>
2021-01-27 02:44:29 +0700

    deleted

--- sipd.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-html_doc = requests.get('https://sukabumikab.sipd.kemendagri.go.id/daerah/main/plan/asmas/2022/22/0')
-soup = BeautifulSoup(html_doc.text, 'html.parser')
-print(soup)
